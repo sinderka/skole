6073ea6dcebb141119bfad3554c36cfa5020776a
sindreka <sindre_e@hotmail.com>
2016-03-19 17:56:17 +0100

    A repository for various projectes

--- FFT/record.m
+++
@@ -1,7 +0,0 @@
- function y = record(sek,Fs)
- aud = audiorecorder(Fs,16,1);
- disp('Recording...');
- recordblocking(aud,sek);
- disp('Recording complete');
- y = getaudiodata(aud);
- end
--- Skole-prosjekter/TMA4205- Prosjekt 2/K2.m
+++
@@ -1,3 +0,0 @@
-function A = K2(n)
-A = spdiags(ones(n-1,1)*[-1 1],[0 1],n-1,n);
-end
